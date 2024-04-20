f21e7fd6c1cb3b41df3f0a25001c1c4d637d84d9
Johan Krogh <johan.alexander.turesson.krogh@randers.dk>
2024-04-20 23:28:53 +0200

    Implementér POST http request til /api/dokument/journaliser

--- src/app.py
+++ src/app.py
@@ -1,14 +1,17 @@
 from flask import Flask, jsonify, request
 from request_validation import validate_request_journaliser_fil
 from sbsys_operations import SBSYSOperations
+import requests, base64, os
 
-sbsys_url = "https://sbsysapi.randers.dk"
-token = ""
+# docker build -t signatur-ansatdata .
+# docker run -d -p 8080:8080 signatur-ansatdata
+sbsys_url = "https://sbsysapitest.randers.dk"
+token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIyZlR0cW5Hamo3M082bElzUmZDdlp4WHYtNjVva0I3WFZDNERRT2JnT3ZNIn0.eyJqdGkiOiJjNTgzNjg5Ny03Y2NhLTQ5ZjAtYTc4Yi1lYTJlODI3MGY5N2YiLCJleHAiOjE3MTM2NzI5MjIsIm5iZiI6MCwiaWF0IjoxNzEzNjQ0MTIyLCJpc3MiOiJodHRwczovL3Nic2lwLXdlYi10ZXN0MDEucmFuZGVycy5kazo4NTQzL2F1dGgvcmVhbG1zL3Nic2lwIiwiYXVkIjoicmFuZGVycy11ZHZpa2xpbmcta2xpZW50Iiwic3ViIjoiYzExNzEzYTItN2M5OC00NmY3LWFiOWItYmM0Y2MyYWViYjZlIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoicmFuZGVycy11ZHZpa2xpbmcta2xpZW50IiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiNjE5NWEwYWUtODk0MC00ZjEwLThmZjItM2QzOWM1ODBlNzkyIiwiYWNyIjoiMSIsImNsaWVudF9zZXNzaW9uIjoiN2IxMzBmMDItMTc0My00YjViLWIzNTEtNDI2NWI0ZmU0OWI1IiwiYWxsb3dlZC1vcmlnaW5zIjpbXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwiYXVkIjoic2JzeXNhcGl0ZXN0LnJhbmRlcnMuZGsiLCJuYW1lIjoiIiwicHJlZmVycmVkX3VzZXJuYW1lIjoicGVyc29uYWxlc2FnZXIifQ.O1huehTbHj23W5KFnZCpSkUmVOorgRYQAlp8Mx5S3PZ3eJiPZzWHmo1n-RuzO7poQDPi4NMbo7uMV99uSbszgcubiXPK4wRx5ePzSxiiiEgrOxVlf73I44HmI6jTHtG7KMf-uqLG11hplVv9F5Kfml-1huwXERjAjVpGZCWQ68r-x5-Moq__JmClh6yq84-VOo8I8lL5FN_ONp6E61IMWvXrpFs2wTAeHX15k5qYPd0EceXkAIUN7BiSdI-kY8241BEQCx-NZACPH2XyzF-NVZvI0SUE9bCVr_VX0lFrZwyO1M22riwYGvq2UV6mQCS1bLuqicOBKxb19nvEOBIw9A"
 app = Flask(__name__)
 
 @app.route('/api/journaliser/fil', methods=['POST'])
 def sbsys_journaliser_fil():
-    print("hello world")
+
     # Check if request contains JSON data
     if not request.is_json:
         return jsonify({"error": "Request must contain JSON data"}), 400
@@ -22,21 +25,24 @@ def sbsys_journaliser_fil():
         return jsonify(error_response), 400  # Return error response if validation fails
 
     # Find newest personalesag based on CPR from request
-    json_data = {
-        "PrimaerPerson": {
-            "CprNummer": request.json.get('cpr')
-        },
-        "SagsTyper": [
-            {
-            "Id": 5
-            }
-        ]
-    }       
+    cpr = request.json.get('cpr')
     sbsys = SBSYSOperations(sbsys_url, token)
-    sag = sbsys.find_newest_personalesag(json_data)
+    sag = sbsys.find_newest_personalesag(cpr)
+    
+    # Check if sag is None
+    if sag is None:
+        return jsonify({"error": "Failed to retrieve search results based on given cpr"}), 500
+
+    # Journalise file 
+    fil = request.json.get('fil')
+    binary_data = base64.b64decode(fil)
+
+    response = sbsys.journalise_file(sag, binary_data)
+    print(response)
+
     # TODO journaliser fil fra request på sagen fra find_newest_personalesag
     # TODO Hvordan skal filen journaliseres? delforløb, navn, type.
-    return jsonify({"success": "Data validated successfully"}), 200
+    return jsonify({"success": "Fil uploaded successfully"}), 200
 
 
 if __name__ == "__main__":
--- src/request_validation.py
+++ src/request_validation.py
@@ -1,4 +1,4 @@
-import base64
+from utils import convert_filestring_to_bytes
 
 def validate_cpr(cpr):
     return True
@@ -6,38 +6,18 @@ def validate_cpr(cpr):
 def validate_file(filestring):
      return convert_filestring_to_bytes(filestring).startswith(b'%PDF')
 
-# Convert a base64 encoded string to file
-def convert_filestring_to_bytes(file_string):
-    try:
-        # Decode base64 string
-        decoded_bytes = base64.b64decode(file_string, validate=True)
-        
-        # Check if the decoded bytes start with the PDF magic number
-        if decoded_bytes.startswith(b'%PDF'):
-            return decoded_bytes, None
-        else:
-            return None, {"error": "File must be a PDF"}
-    except Exception as e:
-        # If an error occurs during decoding or validation, return an error message
-        print(f"Error decoding base64 string: {e}")
-        return None, {"error": "File is not valid. Make sure it is a base64 encoded filestring"}
 
 # validate the request where False results in a Bad Request response
 def validate_request_journaliser_fil(data):  
     # Check if 'cpr', 'fil', and 'sagType' keys exist
-    if not all(key in data for key in ['cpr', 'fil', 'sagType']):
-        return False, {"error": "Request must contain 'cpr', 'fil', and 'sagType' keys"}
+    if not all(key in data for key in ['cpr', 'fil']):
+        return False, {"error": "Request must contain 'cpr' and 'fil' keys"}
 
     # Validate 'cpr' format
     cpr = str(data.get('cpr', ''))  # Convert to string
     if not (len(cpr) == 10 and cpr.isdigit()) and not (len(cpr) == 11 and cpr[:6].isdigit() and cpr[6] == '-' and cpr[7:].isdigit()):
         return False, {"error": "Invalid format for 'cpr'. It should be digits in either 'xxxxxxxxxx' or 'xxxxxx-xxxx' format"}
 
-    # Check 'sagType' format
-    sag_type = data.get('sagType')
-    if not isinstance(sag_type, dict) or 'Id' not in sag_type or not isinstance(sag_type['Id'], int) or sag_type['Id'] != 0:
-        return False, {"error": "Invalid format for 'sagType'. It should be a dictionary with an 'Id' key, e.g., {'Id': 0}"}
-    
     # Validate CPR value
     if not validate_cpr(cpr):
         return False, {"error": "Der eksisterer ingen personalesager på dette CPR"}
